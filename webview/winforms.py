bd1022ee41c43e56c6cd746c8e7108c0643afd80
Roman <roman@flowrl.com>
2017-09-17 01:48:30 +0300

    [WinForms] Fix debug missing argument

--- webview/winforms.py
+++ webview/winforms.py
@@ -245,7 +245,7 @@ def create_window(title, url, width, height, resizable, fullscreen, min_size,
                   confirm_quit, background_color, debug, webview_ready):
     set_ie_mode()
     browser_view = BrowserView(title, url, width, height, resizable, fullscreen,
-                               min_size, confirm_quit, background_color, debug, webview_ready)
+                               min_size, confirm_quit, background_color, webview_ready)
     browser_view.show()
 
 
